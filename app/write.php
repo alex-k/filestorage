c10c425e58b4912d01df98397a473617ee7c99d4
alex <alex@kochetov.com>
2017-08-26 09:29:37 +0300

    rename HashTree generator to OneFile generator

--- app/write.php
+++ app/write.php
@@ -1,12 +1,12 @@
 <?php
 include __DIR__.DIRECTORY_SEPARATOR.'bootstrap.php';
 
-use Alexk\Storage\FileNameGenerator\HashTree;
+use Alexk\Storage\FileNameGenerator\OneFile;
 use Alexk\Storage\FileStorage;
 
 list(, $filename) = $argv + [NULL];
 
 $in = fopen("php://stdin", "rb");
 
-$storage = new FileStorage(new HashTree($filename));
+$storage = new FileStorage(new OneFile($filename));
 $storage->write($in);
